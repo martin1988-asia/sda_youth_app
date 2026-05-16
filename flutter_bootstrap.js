8f950e49c44234580acdf6c50f8289c06a217a2c
martin1988-asia <mbweti@gmail.com>
2026-05-16 19:53:34 +0200

    Deploy fresh build to gh-pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1675629721" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2427054359" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
